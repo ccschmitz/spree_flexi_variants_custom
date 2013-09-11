f8f55267913529ad087ed2168e1c5632a60031e2
Chris Schmitz <ccschmitz@gmail.com>
2013-09-10 22:27:33 -0500

    Update selector for Deface override

--- app/overrides/add_extra_order_details_line_item_description.rb
+++ app/overrides/add_extra_order_details_line_item_description.rb
@@ -5,6 +5,5 @@ Deface::Override.new(:virtual_path => "spree/shared/_order_details",
 
 Deface::Override.new(:virtual_path => "spree/admin/shared/_order_details",
                      :name => "add_extra_order_details_line_item_description_admin",
-                     :insert_bottom => "[data-hook='order_details_line_item_row'] td:first",
+                     :insert_bottom => "[data-hook='stock-contents']",
                      :partial => "spree/shared/extra_order_details_line_item_description")
-
